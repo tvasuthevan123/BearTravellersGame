e96e6f25634bb0cb6a06e5549396f3f1ad8ebd5f
tvasuthevan123@gmail.com <45975730+tvasuthevan123@users.noreply.github.com>
2023-02-02 20:49:29 +0000

    Fix disable on dialogue, fix transport on qtetrigger

--- Assets/Original/Scripts/StoryEngine/QTETrigger.cs
+++ Assets/Original/Scripts/StoryEngine/QTETrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarterAssets;
 using UnityEngine.InputSystem;
 
 public class QTETrigger : MonoBehaviour
@@ -8,17 +9,18 @@ public class QTETrigger : MonoBehaviour
     public GameObject qteObject;
     public static bool qteComplete = false;
 
-    [SerializeField] private Vector3 dialogueLook, dialoguePosition;
+    [SerializeField] private Vector3 dialoguePosition;
     [SerializeField] Transform playerCamera;
     [SerializeField] GameObject player;
     [SerializeField] private GameObject fence1, fence2, fence3, dialogue;
 
     void OnTriggerEnter()
     {
-        if (dialogueLook != Vector3.zero && dialoguePosition != Vector3.zero)
+        player.GetComponent<FirstPersonController>().disableInput = true;
+        if (dialoguePosition != Vector3.zero)
         {
             player.transform.position = dialoguePosition;
-            playerCamera.LookAt(dialogueLook);
+            playerCamera.rotation = Quaternion.Euler(5.6f, 128.5f, -0.029f);
         }
         player.GetComponent<PlayerInput>().DeactivateInput();
         player.GetComponent<HealthItems>().enabled = false;
@@ -33,6 +35,7 @@ public class QTETrigger : MonoBehaviour
     {
         if (qteComplete == true)
         {
+            player.GetComponent<FirstPersonController>().disableInput = false;
             player.GetComponent<PlayerInput>().ActivateInput();
             player.GetComponent<HealthItems>().enabled = true;
             player.GetComponent<PlayerPickup>().enabled = true;
